c1b19dbcf1c3ece9259dda99ef41b3cf3781836b
Laura Braker <thibmorozier@live.nl>
2026-06-13 13:10:27 +0200

    Split SubLink.References to it's own repo

---
+++ GlobalUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace tech.SubLink;
+
+public static class GlobalUtils
+{
+    private const string CSettingsDir = "Settings";
+
+    public static string GetSettingsFilePath(string platformName)
+    {
+        if (!Directory.Exists(CSettingsDir))
+            Directory.CreateDirectory(CSettingsDir);
+
+        return Path.Combine(CSettingsDir, $"{platformName}.json");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# SubLink.References
+
+ILRepacked DLL containing shared dependencies between platforms. (And Twitch)
+
+Mostly useless on it's own, all platforms should use this to be able to wok with SubLink itself.
